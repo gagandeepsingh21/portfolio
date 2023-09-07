fb2c351e89d9503b1d1bf3332e21efc16b11aae5
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-09-07 16:49:23 +0300

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,11 +9,11 @@ const Contact = () => {
                 <p className='py-6'>Submit the form below to get in touch with me</p>
             </div>
             <div className='flex justify-center items-center'>
-                <form name="contact" method="POST" data-netlify="true" className='flex flex-col w-full md:w-1/2'>
+                <form name="contact" method="POST" className='flex flex-col w-full md:w-1/2' netlify>
                     <input type="text" name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                     <input type="email" name='email' placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                     <textarea name='message' rows={5} placeholder='Enter your message here...' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
-                    <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex-items-center rounded-md hover:scale-110 duration-300'>Let's chat</button>
+                    <button type="submit" className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex-items-center rounded-md hover:scale-110 duration-300'>Let's chat</button>
                 </form>
             </div>
         </div>
