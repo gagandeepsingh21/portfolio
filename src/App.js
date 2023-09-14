be663598e9a1fbec5f5a25eae8646bf99563b678
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-09-14 12:27:56 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Social from "./components/Social";
 import About from "./components/About";
-import Portifolio from "./components/Portifolio";
+import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <NavBar/>
       <Home/>
       <About />
-      <Portifolio />
+      <Portfolio />
       <Experience /> 
       <Contact />
 
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import fooddonation from '../assets/portfolio/fooddonation.png'
+import netflixclone from '../assets/portfolio/netflixclone.png'
+const Portfolio = () => {
+    const portfolios = [
+        {
+          id: 1,
+          src: fooddonation,
+          href:'https://github.com/gagandeepsingh21/food_donation_is'
+        },
+        {
+          id: 2,
+          src: netflixclone,
+          href:'https://netflix-clone-1099.netlify.app/'
+
+        },
+      ];
+  return (
+    <div name='Portfolio' className='w-full h-Screen bg-gradient-to-b from-black to-gray-800 text-white'>
+    <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'> 
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                Portfolio
+            </p>
+            <p className='py-6'>Check out some of my work right here.</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+        {portfolios.map(({ id, src, href }) => (
+            <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
+                <div className='flex flex-center justify-center'>
+                    <a href={href} target='_blank' className='text-center px-6 py-3 m-4 duration-200 hover:scale-105' rel="noreferrer">View</a>
+                </div>
+            </div>
+         ))}
+        </div>
+
+    </div>
+</div>  
+)
+}
+
+export default Portfolio
