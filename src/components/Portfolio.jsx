d79d110e35e02974d9e077a807d7633c3efa0f01
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-09-16 20:07:54 +0300

    portfolio additons and favicon changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import fooddonation from '../assets/portfolio/fooddonation.png'
 import netflixclone from '../assets/portfolio/netflixclone.png'
+import qrcodegenerator from '../assets/portfolio/qrcodegenerator.png'
+import hostelmanagement from '../assets/portfolio/hostelmanagement.png'
 const Portfolio = () => {
     const portfolios = [
         {
           id: 1,
+          src: hostelmanagement,
+          href:'https://github.com/gagandeepsingh21/ISP1'
+        },
+        {
+          id: 2,
           src: fooddonation,
           href:'https://github.com/gagandeepsingh21/food_donation_is'
         },
         {
-          id: 2,
+          id: 3,
           src: netflixclone,
           href:'https://netflix-clone-1099.netlify.app/'
+
+        },
+        {
+          id: 4,
+          src: qrcodegenerator,
+          href:'https://qr-code-generator-21.netlify.app/'
 
         },
       ];
